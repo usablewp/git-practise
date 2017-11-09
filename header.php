c5ccd8fa853a23d722be9494e2e7cca3b7d29792
Naresh Devineni <nareshdevineni@hotmail.com>
2017-11-09 22:07:37 +0530

    Add description meta tag

--- header.php
+++ header.php
@@ -12,6 +12,7 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <meta name="description" content="The High Quality Theme">
 
     <!-- Important header tags -->
     <link rel="profile" href="http://gmpg.org/xfn/11">
